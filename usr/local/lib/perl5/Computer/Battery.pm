417f342d999fe652a9b301f38eb1761bfc485542
Troy Will <troydwill@gmail.com>
2011-06-14 13:51:04 -0700

    Battery.pm: remaining_capacity method

--- usr/local/lib/perl5/Computer/Battery.pm
+++ usr/local/lib/perl5/Computer/Battery.pm
@@ -1,117 +1,119 @@
 package Computer::Battery;
 $VERSION = 1.00;
 use strict;
-use vars qw( $AUTOLOAD );   # Keep 'use strict' happy
+use vars qw( $AUTOLOAD );    # Keep 'use strict' happy
 use Carp;
 
 {
-# Encapsulated class data
 
-	my %_attr_data =	#	DEFAULT	ACCESSIBILITY
-		(	_remaining_capacity => ['???',	'read']
-		);
+    # Encapsulated class data
 
-	my $_count = 0;
-# Class methods, to operate on encapsulated class data
+    my %_attr_data =         #	DEFAULT	ACCESSIBILITY
+      ( _remaining_capacity => [ '???', 'read' ] );
 
-	# Is a specified object attribute accessible in a given mode
-	sub _accessible
-	{
-		my ($self, $attr, $mode) = @_;
-		$_attr_data{$attr}[1] =~ /$mode/
-	}
+    my $_count = 0;
 
-	# Classwide default value for a specified object attribute 
-	sub _default_for
-	{
-		my ($self, $attr) = @_;
-		$_attr_data{$attr}[0];
-	}
+    # Class methods, to operate on encapsulated class data
 
-	# List of names of all specified object attributes 
-	sub _standard_keys 
-	{
-		keys %_attr_data;
-	}
+    # Is a specified object attribute accessible in a given mode
+    sub _accessible {
+        my ( $self, $attr, $mode ) = @_;
+        $_attr_data{$attr}[1] =~ /$mode/;
+    }
 
-	# Retrieve object count
-	sub get_count
-	{
-		$_count;
-	}
+    # Classwide default value for a specified object attribute
+    sub _default_for {
+        my ( $self, $attr ) = @_;
+        $_attr_data{$attr}[0];
+    }
 
-	# Private count increment/decrement methods
-	sub _incr_count { ++$_count }
-	sub _decr_count { --$_count }
-					
-}
-# Constructor may be called as a class method
-# (in which case it uses the class's default values),
-# or an object method
-# (in which case it gets defaults from the existing object)
+    # List of names of all specified object attributes
+    sub _standard_keys {
+        keys %_attr_data;
+    }
 
-sub new
-{
-	my ($caller, %arg) = @_;
-	my $caller_is_obj = ref($caller);
-	my $class = $caller_is_obj || $caller;
-	my $self = bless {}, $class;
-	foreach my $membername ( $self->_standard_keys() )
-	{
-		my ($argname) = ($membername =~ /^_(.*)/);
-		if (exists $arg{$argname})
-			{ $self->{$membername} = $arg{$argname} }
-		elsif ($caller_is_obj)
-			{ $self->{$membername} = $caller->{$membername} }
-		else
-			{ $self->{$membername} = $self->_default_for($membername) }
-	}
-	$self->_incr_count();
-	return $self;
-}
+    # Retrieve object count
+    sub get_count {
+        $_count;
+    }
 
-# Destructor adjusts class count
-sub DESTROY
-{
-	$_[0]->_decr_count();
-}
-# get or set room&shelf together
+    sub remaining_capacity {
+      my ($self, $remaining_capacity) = @_;
+      $self->{_remaining_capacity} = 100;
+      return $self->{_remaining_capacity};
+    }
 
-sub get_location	{ ($_[0]->get_room(), $_[0]->get_shelf()) }
-sub set_location
-{
-	my ($self, $room, $shelf) = @_;
-	$self->set_room($room) if $room;
-	$self->set_shelf($shelf) if $shelf;
-	return;
-}
+        # Private count increment/decrement methods
+        sub _incr_count { ++$_count }
+        sub _decr_count { --$_count }
 
-# Implement other get_… and set_… methods (create as necessary)
+    }
 
-sub AUTOLOAD
-{
-	no strict "refs";
-	my ($self, $newval) = @_;
+    # Constructor may be called as a class method
+    # (in which case it uses the class's default values),
+    # or an object method
+    # (in which case it gets defaults from the existing object)
 
-	# Was it a get_… method?
-	if ($AUTOLOAD =~ /.*::get(_\w+)/ && $self->_accessible($1,'read'))
-	{
-		my $attr_name = $1;
-		*{$AUTOLOAD} = sub { return $_[0]->{$attr_name} };
-		return $self->{$attr_name}
-	}
+    sub new {
+        my ( $caller, %arg ) = @_;
+        my $caller_is_obj = ref($caller);
+        my $class         = $caller_is_obj || $caller;
+        my $self          = bless {}, $class;
+        foreach my $membername ( $self->_standard_keys() ) {
+            my ($argname) = ( $membername =~ /^_(.*)/ );
+            if ( exists $arg{$argname} ) {
+                $self->{$membername} = $arg{$argname};
+            }
+            elsif ($caller_is_obj) {
+                $self->{$membername} = $caller->{$membername};
+            }
+            else { $self->{$membername} = $self->_default_for($membername) }
+        }
+        $self->_incr_count();
+        return $self;
+    }
 
-	# Was it a set_… method? 
-	if ($AUTOLOAD =~ /.*::set(_\w+)/ && $self->_accessible($1,'write'))
-	{
-		my $attr_name = $1;
-		*{$AUTOLOAD} = sub { $_[0]->{$attr_name} = $_[1] };
-		$self->{$1} = $newval;
-		return
-	}
-	
-	# Must have been a mistake then…
-	croak "No such method: $AUTOLOAD";
-}
+    # Destructor adjusts class count
+    sub DESTROY {
+        $_[0]->_decr_count();
+    }
 
-1;
+    # get or set room&shelf together
+
+    sub get_location { ( $_[0]->get_room(), $_[0]->get_shelf() ) }
+
+    sub set_location {
+        my ( $self, $room, $shelf ) = @_;
+        $self->set_room($room)   if $room;
+        $self->set_shelf($shelf) if $shelf;
+        return;
+    }
+
+    # Implement other get_… and set_… methods (create as necessary)
+
+    sub AUTOLOAD {
+        no strict "refs";
+        my ( $self, $newval ) = @_;
+
+        # Was it a get_… method?
+        if ( $AUTOLOAD =~ /.*::get(_\w+)/ && $self->_accessible( $1, 'read' ) )
+        {
+            my $attr_name = $1;
+            *{$AUTOLOAD} = sub { return $_[0]->{$attr_name} };
+            return $self->{$attr_name};
+        }
+
+        # Was it a set_… method?
+        if ( $AUTOLOAD =~ /.*::set(_\w+)/ && $self->_accessible( $1, 'write' ) )
+        {
+            my $attr_name = $1;
+            *{$AUTOLOAD} = sub { $_[0]->{$attr_name} = $_[1] };
+            $self->{$1} = $newval;
+            return;
+        }
+
+        # Must have been a mistake then…
+        croak "No such method: $AUTOLOAD";
+    }
+
+    1;
